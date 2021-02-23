c0f367f6a99d4a33c03a82bf812a977217abda43
bioinfo <sergio.fern1994@gmail.com>
2021-02-23 18:56:44 +0100

    features: new features for Simple_VCF_viewer

--- Simple_VCF_viewer/app.R
+++ Simple_VCF_viewer/app.R
@@ -45,8 +45,9 @@ ui <- fluidPage(
         mainPanel(
             h3('Variant Calling File:'),
             DTOutput(outputId = 'vcf_table'),
-
-                column(width = 6,
+            h3('Common Operations:'),
+            fluidRow(
+                column(width = 3,
                        wellPanel(
                            
                            h4('Download selected variants'),
@@ -62,12 +63,20 @@ ui <- fluidPage(
                             h4('Variants'),
                             textOutput(outputId = 'display_variant', inline = TRUE)
                            
-                       ) # Well Panel end bracket
-                       
-                       )# Column end bracket
+                       )) # Well Panel end bracket
+                       ),
+            fluidRow(
+                column(width = 5,
+                wellPanel(
+                h4('Recalculate Alternate Frequency (AF) values'),
+                textOutput(outputId = 'AF_explanation'),
+                br(),
+                actionButton('Recalculate_AF',label = 'Recalculate AF', icon = icon("redo-alt"))
                 
+            )))# fluidRow & wellPanel & column end bracket
+            
 
-        ) # Main Pan el end bracket
+        ) # Main Panel end bracket
     ) # SideBar layout end bracket
 ) # fluidPage end bracket
 
@@ -75,23 +84,23 @@ ui <- fluidPage(
 server <- function(input, output) {
     
     datos <- reactiveValues()
-
-    vcf_r <- reactive({read.vcfR(input$vcf_file$datapath)})
-    vcf_tidy <- reactive({vcfR2tidy(vcf_r())})
-    
-    vcf.meta <- reactive({vcf_tidy()$meta})
-    vcf.table <- reactive({vcf_tidy()$fix})
-    vcf.gt <- reactive({vcf_tidy()$gt})
-    
-    vcf_reference <- reactive({
-        if(is.null(input$vcf_reference)){return('no-reference')
-            }else{return(input$vcf_reference$name)}
-    })
-    
-    reference_type <- reactive({return(str_sub(vcf_reference(), 1,2))})
-    reference_ID <- reactive({return(str_split(vcf_reference(), '\\.')[[1]][1])})
     
     observeEvent(input$run_vcf_reader, {
+        
+        vcf_r <- reactive({read.vcfR(input$vcf_file$datapath)})
+        datos$vcf_tidy <- vcfR2tidy(vcf_r())
+        
+        vcf.meta <- reactive({datos$vcf_tidy$meta})
+        vcf.table <- reactive({datos$vcf_tidy$fix})
+        vcf.gt <- reactive({datos$vcf_tidy$gt})
+        
+        vcf_reference <- reactive({
+            if(is.null(input$vcf_reference)){return('no-reference')
+            }else{return(input$vcf_reference$name)}
+        })
+        
+        reference_type <- reactive({return(str_sub(vcf_reference(), 1,2))})
+        reference_ID <- reactive({return(str_split(vcf_reference(), '\\.')[[1]][1])})
         
         output$vcf_table <- renderDT(extensions = c('Buttons','FixedColumns','Scroller','ColReorder'), 
                                      options = list(dom = 'Bfrtip', 
@@ -169,6 +178,12 @@ server <- function(input, output) {
     #################################.
     variant_file_name <- reactive({'variant_list.txt'})
     callModule(downloadFile, 'download_variants', data = variants_to_download, name = variant_file_name)
+    
+############# recalculate AF #######################
+    #################################.
+    
+    output$AF_explanation <- renderText("Alternate frequency commonly needs to be recalculated, some variant callers that assume diploid or haploid distribution of the data, and in consequence they print 0,0.5 or 1 in the AF column. Despite the real AF value being something in between.")
+    observeEvent(input$Recalculate_AF, {datos$vcf_tidy$fix <- datos$vcf_tidy$fix %>% mutate(AF = signif(as.numeric(AO)/DP,digits = 2))})
 }
 
 
